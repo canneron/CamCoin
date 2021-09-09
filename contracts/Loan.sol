261864a43d884ef5ca9cf4f32315fb51328ae1a2
canneron <mcgreevycameron@gmail.com>
2021-09-10 00:47:01 +0100

    Added new functions, needs to be reworked to include arrays to track open and closed loans

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -8,23 +8,32 @@ contract Loan {
   mapping (uint => uint) amountLent;
   mapping (uint => uint) loanInterest;
   mapping (uint => uint) collateralOwed;
+  mapping (uint => address) loanTime;
   // Store data for loans being requested
   mapping (uint => address) requestedBy;
   mapping (uint => uint) amountRequested;
   mapping (uint => uint) interestRequested;
   mapping (uint => uint) collateralRequested
-  ;
-  mapping (address => mapping(address => uint)) loanedToAddress;
-  mapping (address => mapping(address => uint)) outstandingInterest;
+  mapping (uint => address) loanTimeRequested;
+  // Store data for taken loans
+  mapping (uint => uint) loanTakenTime;
+  mapping (uint => uint) loanTakenTime;
+
+  struct LoanTaken { address addr; }
+  struct LoanOffered { address addr; }
+
+  mapping (uint256 => LoanTaken) internal loansTakenByAddress;
+  mapping (uint256 => LoanOffered) internal loansOfferedByAddress;
 
   event loanOffered(uint256 _amount, uint256 _interest, uint256 _collateral);
   event loanOffered(uint256 _amount, uint256 _interest, uint256 _collateral);
   event loanTaken(uint256 _amount, uint256 _interest, uint256 _collateral);
+  event loanOffered(uint256 _amount, uint256 _interest, uint256 _collateral);
 
   uint256 private loanId = 0;
   uint256 private requestId = 0;
 
-  function lend(address lender, uint256 amount, uint256 interest) external {
+  function lend(address lender, uint256 amount, uint256 interest, uint time) external {
     CamCoin lenderToken = CamCoin(lender);
 
     uint allowance = lenderToken.allowance(lender, msg.sender);
@@ -37,17 +46,18 @@ contract Loan {
     amountLent[loanId] = amount;
     loanInterest[loanId] = interest;
     collateralOwed[loanId] = collateral;
+    loanTime[loanId] = time;
+
     loanId += 1;
     lenderToken.transferFrom(lender, address(this), amount);
     emit loanOffered(amount, interest, collateral);
   }
 
-  function requestLoan(address borrower, uint256 amount, uint interest, uint collateral) external {
+  function requestLoan(address borrower, uint256 amount, uint interest, uint collateral, uint time) external {
     CamCoin borrowerToken = CamCoin(borrower);
 
-    uint maxLoan = collateral * 100000;
+    uint maxLoan = collateral * 10;
     require(borrowerToken.balanceOf(borrower) >= collateral);
-    require(amount <= totalLoanPool, "You cannot loan more than is available");
     require(amount <= maxLoan, "You have insufficient collateral for this loan");
     require(amount >= 0, "You must loan a non 0 amount");
     require(amount <= 100000000, "Loan cannot exceed max supply");
@@ -56,12 +66,37 @@ contract Loan {
     amountRequested[requestId] = amount;
     interestRequested[requestId] = interest;
     collateralRequested[requestId] = collateral;
+    loanTimeRequested[requestId] = time;
     requestId += 1;
+
+    borrowerToken.transferFrom(borrower, address(this), amount);
     emit loanRequested(amount, interest, collateral);
   }
 
-  function takeLoan(address borrower, uint takenLoanId) external payable {
 
+
+  function takeLoan(address borrower, uint takenLoanId) external payable {
+    CamCoin borrowerToken = CamCoin(borrower);
+    CamCoin lenderToken = CamCoin(lentBy[takenLoanId]);
+
+    loansTakenByAddress[takenLoanId].addr = borrower;
+    loanTakenTime[takenLoanId] = now;
+    require(borrowerToken.balanceOf(borrower) >= collateralOwed[takenLoanId]);
+    borrowerToken.transferFrom(borrower, address(this), collateralOwed[takenLoanId]);
+    lenderToken.transfer(borrower, amountLent[takenLoanId]);
+    emit loanTaken(uint256 amountLent[takenLoanId], uint256 loanInterest[takenLoanId], uint256 loanInterest[takenLoanId]);
+  }
+
+  function offerLoan(address lender, uint requestTakenId) {
+    CamCoin borrowerToken = CamCoin(requestedBy[requestTakenId]);
+    CamCoin lenderToken = CamCoin(lender);
+
+    loansOfferedByAddress[requestTakenId].addr = lender;
+    loanOfferedTime[requestTakenId] = now;
+    require(lenderToken.balanceOf(lender) >= amountRequested[requestTakenId]);
+    borrowerToken.transferFrom(requestedBy[requestTakenId], address(this), collateralRequested[requestTakenId]);
+    lenderToken.transfer(requestedBy[requestTakenId], amountRequested[requestTakenId]);
+    emit loanOffered(uint256 amountRequested[requestId], uint256 interestRequested[requestTakenId], uint256 collateralRequested[requestTakenId]);
   }
 
   function repayLoan
